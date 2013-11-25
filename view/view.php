8e51e9bce865a6201f18aaa0c4aed3de04b4ba24
derpixler <rene@die-pixler.de>
2013-11-25 09:40:31 +0100

    add viw

---
+++ view/view.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Feature Name:	view
+ * Version:			0.1
+ * Author:			renereimann
+ * Author URI:		http://www.rene-reimann.de/
+ * Licence:
+ */
+
+namespace Play\view;
+
+class View {}
